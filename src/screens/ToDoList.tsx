69b90c24e43f9b0de50bd6357dbc8ff1e99093ab
Jonas Dhein <jonas@fortalezatec.com.br>
2024-11-18 21:55:12 -0300

    Aula 5 - Parte 2

--- src/screens/ToDoList.tsx
+++ src/screens/ToDoList.tsx
@@ -1,9 +1,44 @@
-import { SafeAreaView, Text } from "react-native"
+import { SafeAreaView, Text, TextInput } from "react-native"
+import { theme } from "../themes/theme";
+import { useEffect, useState } from "react";
 
 export const ToDoList = () => {
-    return(
-        <SafeAreaView>
-            <Text>TO DO LIST!!!</Text>
+
+    const [input, setInput] = useState<string>('');
+    const [todoList, setTodoList] = useState<string[]>([]);
+
+    const save = (text: string) => {
+        
+        const newList = [...todoList, text];
+
+        setTodoList(newList);
+    }
+
+    useEffect(() => {
+        console.log('INPUT', input)
+    }, [input])
+
+    return (
+        <SafeAreaView style={theme.container}>
+            <TextInput
+                style={theme.input}
+                onChangeText={(value) => setInput(value)}
+                value={input}
+                onSubmitEditing={() => {
+                    save(input);
+                    setInput('');
+                }}
+            />
+
+            <Text style={theme.label}>Lista de Tarefas:</Text>
+            
+            {
+                todoList.map((tarefa, index) => (
+                    <Text
+                        style={theme.listItem}
+                        key={index}>{tarefa}</Text>
+                ))
+            }
         </SafeAreaView>
     )
 }
--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const colors = {
     background: '#CFD8DC',
+    backgroundInut: '#FFF3E0',
     blue: '#2196F3',
     orange: '#FF9800',
     white: '#FFFFFF'
@@ -10,8 +11,7 @@ export const colors = {
 export const theme = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        margin: 12
     },
     button: {
         backgroundColor: colors.blue,
@@ -26,5 +26,22 @@ export const theme = StyleSheet.create({
     },
     marginBottom: {
         marginBottom: 12
+    },
+    input: {
+        backgroundColor: colors.backgroundInut,
+        fontSize: 16,
+        padding: 12,
+        borderWidth: 0.5,
+        borderColor: colors.orange,
+        borderRadius: 8
+    },
+    label: {
+        fontSize: 20,
+        fontWeight: 500,
+        marginVertical: 8
+    },
+    listItem: {
+        fontSize: 16,
+        marginBottom: 8
     }
 });
